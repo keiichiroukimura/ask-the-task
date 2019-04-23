10786bfd37e6e8744c6ea338bde41f03094cfbf0
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-04-23 21:02:24 +0900

    一覧作成日テスト作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :task do
-    title { "資料作成" }
-    content { "A会社プレゼン用資料作成" }
+    title { "test_task_01" }
+    content { "testtesttest" }
   end
   factory :second_task, class: Task do
-    title { "打ち合わせ" }
-    content { "床下配管工事日程調整打ち合わせ" }
+    title { "test_task_02" }
+    content { "samplesample" }
   end
 end
