19ad2cc0271a723178e6e9cc9381027e91d6a2e9
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-04-27 10:13:29 +0900

    　テスト修正、show画面レイアウト中央寄せ

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -27,22 +27,21 @@ RSpec.feature "タスク管理機能", type: :feature do
   end
 
   scenario "タスク詳細のテスト" do
-    Task.create!(id: '1', title: 'test_task_01', content: 'testtesttest', deadline: '4月30日', priority: '1', status: 'a')
+    Task.create!(id: '1', title: 'test_task_01', content: 'testtesttest',deadline: Date.today, priority: '1', status: 'a')
     
     visit task_path(1)
   
     expect(page).to have_content 'test_task_01'
     expect(page).to have_content 'testtesttest'
-    expect(page).to have_content '4月30日'
+    expect(page).to have_content Date.today
     expect(page).to have_content '1'
     expect(page).to have_content 'a'
-    
+    save_and_open_page
   end
 
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
     # backgroundに必要なタスクデータの作成処理が書かれているので、ここで書く必要がない
     visit tasks_path
-    save_and_open_page
     # タスクが作成日時の降順に並んでいるかのテスト
     #'終了期限でソートする'ボタンもテーブルの中に入っているので、'test_task_02'はテーブルの２番目の値になる。
     value = all("table tr")[2]
@@ -58,24 +57,13 @@ RSpec.feature "タスク管理機能", type: :feature do
     # expect(all(".task-item__title").map(&:text)).to eq ts
   end
   scenario "終了期限の降順に並び替えられたタスク一覧ページが出現するかのテスト" do
-    visit new_task_path
-    
-    fill_in 'new_title', with: 'test_task_01'
-    fill_in 'new_content', with: 'testtesttest'
-    fill_in 'new_deadline', with: 'DateTime'
-    click_on '登録する'
-    expect(page).to have_content 'test_task_01'
-    expect(page).to have_content 'testtesttest'
-    expect(page).to have_content 'DateTime'
     
     visit tasks_path
     
     click_on '終了期限でソートする'
     
     deadline = all("table tr")[2]
-    expect(deadline).to have_content '4月24日'
-    deadline = all("table tr")[3]
-    expect(deadline).to have_content '4月25日'
+    expect(deadline).to have_content "test_task_01"
   end
 end
 #"タスクが作成日時の降順に並んでいるかのテスト" 
